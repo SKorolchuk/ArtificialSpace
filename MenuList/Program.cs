3636f9b31a3246b176563f02aa32d85b5558de24
Sergey Korolchuk <sergey.korolchuk.dev@gmail.com>
2012-07-26 22:48:07 +0300

    Added initial realization of Menu class
    Extended realization of this class... Again :)

---
+++ MenuList/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace MenuList
+{
+    class Program
+    {
+        private static Action<string> Behavior;  
+        static void Main(string[] args)
+        {
+            XDocument doc = XDocument.Load("Menu.xml");
+            foreach (var s in from s in doc.Element("Menu").Elements() select s)
+            {
+                switch (s.Attribute("behavior").Value)
+                {
+                    case "LUNCH":
+                        Behavior = str =>
+                                            {
+                                                 Console.WriteLine("Now launched {0}", str);
+                                            }; 
+                        break;
+                    case "EXIT":
+                        Behavior = str =>
+                        {
+                            Console.WriteLine("Now exit {0}", str);
+                        };
+                        break;
+                    default:
+                        break;
+                }
+                Behavior(s.Attribute("name").Value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
